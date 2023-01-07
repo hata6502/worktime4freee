46b8f8ef0a931fe4750d9cf0df770c61fce520eb
Tomoyuki Hata <7702653+hata6502@users.noreply.github.com>
2023-01-07 16:19:09 +0900

    .

--- content.js
+++ content.js
@@ -1,25 +1,13 @@
-// DOMが描画されるまで待つ
-const waitForElement = (selector, callback, intervalMs, timeoutMs) => {
-    const startTimeInMs = Date.now();
-    findLoop();
-
-    function findLoop() {
-        if (document.querySelector(selector) != null) {
-            callback();
-            return;
-        } else {
-            setTimeout(() => {
-                if (timeoutMs && Date.now() - startTimeInMs > timeoutMs) return;
-                findLoop();
-            }, intervalMs);
-        }
-    }
-}
-
-// 「打刻時刻の修正」ボタンを押したときに走る処理
+let cleanupRows;
+// 表の変化があったときに走る処理
 const func = () => {
-    // テーブルが表示されるまで待ってから計算して表示する
-    waitForElement(".vb-listTable__table", () => {
+    console.log("a");
+    mutationObserver.disconnect();
+    cleanupRows?.();
+    cleanupRows = undefined;
+
+    const taskDialog = document.querySelector(".vb-taskDialog");
+    if (taskDialog) {
         // 日付をフォーマットする関数
         const dateFormat = {
             _fmt: {
@@ -41,8 +29,8 @@ const func = () => {
         let workAmount = 0;
         let restAmount = 0;
         let today = dateFormat.format(new Date(), 'yyyy-MM-dd');
-        const firstCaptionNum = Number(document.getElementsByClassName("vb-tableListCell__text")[0].id.replace(/[^0-9]/g, ''));
-        // h:i 形式のstringををDate型に変換。24:00を超える場合も補正
+        const firstCaptionNum = Number(taskDialog.getElementsByClassName("vb-tableListCell__text")[0].id.replace(/[^0-9]/g, ''));
+        // h:i 形式のstringをDate型に変換。24:00を超える場合も補正
         const convertDate = (timeStr) => {
             return Number(timeStr.slice(0, 2)) < 24
                 ? new Date(`${today} ${timeStr}`)
@@ -78,7 +66,7 @@ const func = () => {
         }
 
         // テーブルの末尾に行を追加する処理
-        const tableElem = document.getElementsByClassName('vb-listTable__table')[0];
+        const tableElem = taskDialog.getElementsByClassName('vb-listTable__table')[0];
         const insertRow = (textList) => {
             const row = tableElem.tBodies[0].insertRow(-1);
             row.classList.add("vb-tableListRow");
@@ -89,15 +77,25 @@ const func = () => {
                     cellElem.classList.add("vb-tableListCell");
                 }
             });
+            return row;
         }
-        insertRow(["", "勤務合計", `${Math.floor(workAmount / 60)}h ${workAmount % 60}m`, ""]);
-        insertRow(["", "休憩合計", `${Math.floor(restAmount / 60)}h ${restAmount % 60}m`, ""]);
-    }, 100, 10000);
-}
+        const workAmountRow = insertRow(["", "勤務合計", `${Math.floor(workAmount / 60)}h ${workAmount % 60}m`, ""]);
+        const restAmountRow = insertRow(["", "休憩合計", `${Math.floor(restAmount / 60)}h ${restAmount % 60}m`, ""]);
 
-// 「打刻時刻の修正」ボタンが現れたらクリックイベントを付与する
-waitForElement(".vb-button--appearanceTertiary", () => {
-    const button = document.getElementsByClassName('vb-button--appearanceTertiary')[0];
-    button.onclick = func;
-}, 100, 10000)
+        cleanupRows = () => {
+            workAmountRow.remove();
+            restAmountRow.remove();
+        }
+    }
 
+    observe();
+};
+
+const mutationObserver = new MutationObserver(func);
+const observe = () => {
+    mutationObserver.observe(
+        document.body,
+        { subtree: true, childList: true, characterData: true }
+    );
+};
+observe();
